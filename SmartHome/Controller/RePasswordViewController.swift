2957c7b508a3f5afd8e4a81b7d8f848bd597984d
maciejwolejko <wolejko92@gmail.com>
2020-04-10 13:59:48 +0100

    Added private access to initialized objects

--- SmartHome/Controller/RePasswordViewController.swift
+++ SmartHome/Controller/RePasswordViewController.swift
@@ -2,8 +2,8 @@
 //  RePasswordViewController.swift
 //  SmartHome
 //
-//  Created by Maciej Wołejko on 06/03/2020.
-//  Copyright © 2020 Maciej Wołejko. All rights reserved.
+//  Created by MW on 06/03/2020.
+//  Copyright © 2020 MW. All rights reserved.
 //
 
 import UIKit
@@ -12,66 +12,66 @@ class RePasswordViewController: UIViewController, UITextFieldDelegate {
 
     let screen = UIScreen.main.bounds
     
-     lazy var logoImage: UIImageView = {
-           let li = UIImageView()
-           li.contentMode = .scaleAspectFit
-           li.image = UIImage(named: "home")?.withRenderingMode(.alwaysTemplate)
-           li.tintColor = UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1)
-           li.translatesAutoresizingMaskIntoConstraints = false
-           return li
+     private lazy var logoImage: UIImageView = {
+           let imageView = UIImageView()
+           imageView.contentMode = .scaleAspectFit
+           imageView.image = UIImage(named: "home")?.withRenderingMode(.alwaysTemplate)
+           imageView.tintColor = UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1)
+           imageView.translatesAutoresizingMaskIntoConstraints = false
+           return imageView
        }()
        
-       lazy var nameLabel: UILabel = {
-           let nl = UILabel()
-           nl.text = "Smart Home"
-           nl.font = .systemFont(ofSize: screen.height * 0.04)
-           nl.textColor = UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1)
-           nl.translatesAutoresizingMaskIntoConstraints = false
-           return nl
+    private lazy var nameLabel: UILabel = {
+           let label = UILabel()
+           label.text = "Smart Home"
+           label.font = .systemFont(ofSize: screen.height * 0.04)
+           label.textColor = UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1)
+           label.translatesAutoresizingMaskIntoConstraints = false
+           return label
        }()
        
-       lazy var subNameLabel: UILabel = {
-           let sn = UILabel()
-           sn.text = "by mwsoftware"
-           sn.textAlignment = .center
-           sn.font = .systemFont(ofSize: screen.height * 0.02)
-           sn.textColor = UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1)
-           sn.translatesAutoresizingMaskIntoConstraints = false
-           return sn
+    private lazy var subNameLabel: UILabel = {
+           let label = UILabel()
+           label.text = "by mwsoftware"
+           label.textAlignment = .center
+           label.font = .systemFont(ofSize: screen.height * 0.02)
+           label.textColor = UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1)
+           label.translatesAutoresizingMaskIntoConstraints = false
+           return label
        }()
     
-    lazy var emailLabel: UILabel = {
-        let el = UILabel()
-        el.text = "Email"
-        el.textAlignment = .left
-        el.textColor = .white
-        el.font = .systemFont(ofSize: 13)
-        el.translatesAutoresizingMaskIntoConstraints = false
-        return el
+    private lazy var emailLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Email"
+        label.textAlignment = .left
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 13)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
     }()
     
-    lazy var emailTextField : UITextField = {
-        let et = UITextField()
-        et.layer.borderWidth = 1
-        et.layer.cornerRadius = 10
-        et.textAlignment = .center
-        et.delegate = self
-        et.layer.borderColor = UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1).cgColor
-        et.layer.backgroundColor = UIColor(red: 53/255, green: 42/255, blue: 129/255, alpha: 1).cgColor
-        et.attributedPlaceholder = .init(string: "jan.kowalski@gmail.com", attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1)])
-        et.translatesAutoresizingMaskIntoConstraints = false
-        return et
+    private lazy var emailTextField : UITextField = {
+        let textField = UITextField()
+        textField.layer.borderWidth = 1
+        textField.layer.cornerRadius = 10
+        textField.textAlignment = .center
+        textField.delegate = self
+        textField.layer.borderColor = UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1).cgColor
+        textField.layer.backgroundColor = UIColor(red: 53/255, green: 42/255, blue: 129/255, alpha: 1).cgColor
+        textField.attributedPlaceholder = .init(string: "jan.kowalski@gmail.com", attributes: [NSAttributedString.Key.foregroundColor: UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1)])
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
     }()
     
-    lazy var sendNewPasswordButton : UIButton = {
-        let lb = UIButton(type: .system)
-        lb.setTitle("Send Password", for: .normal)
-        lb.setTitleColor(.white, for: .normal)
-        lb.layer.backgroundColor = UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1).cgColor
-        lb.layer.cornerRadius = 10
-        lb.addTarget(self, action: #selector(sendNewPasswordButtonPressed), for: .touchUpInside)
-        lb.translatesAutoresizingMaskIntoConstraints = false
-        return lb
+    private lazy var sendNewPasswordButton : UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Send Password", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.layer.backgroundColor = UIColor(red: 27/255, green: 183/255, blue: 233/255, alpha: 1).cgColor
+        button.layer.cornerRadius = 10
+        button.addTarget(self, action: #selector(sendNewPasswordButtonPressed), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
     
      override func viewDidLoad() {
@@ -80,18 +80,17 @@ class RePasswordViewController: UIViewController, UITextFieldDelegate {
         view.backgroundColor = UIColor(red: 41/255, green: 40/255, blue: 102/255, alpha: 1)
         navigationController?.navigationBar.barTintColor = UIColor(red: 41/255, green: 40/255, blue: 102/255, alpha: 1)
         navigationItem.setHidesBackButton(true, animated: true)
+        setupObjects()
         
-         setupObjects()
      }
     
-     // MARK: - ButtonPressed Function
      @objc private func sendNewPasswordButtonPressed() {
          navigationController?.pushViewController(LoginViewController(), animated: true)
      }
-     
+    
     func setupObjects() {
 
-        [logoImage, nameLabel, subNameLabel, emailLabel, emailTextField, sendNewPasswordButton].forEach({view.addSubview($0)})
+        [logoImage, nameLabel, subNameLabel, emailLabel, emailTextField, sendNewPasswordButton].forEach{view.addSubview($0)}
         
         logoImage.anchor(top: view.safeAreaLayoutGuide.topAnchor, leading: view.leadingAnchor, bottom: nil, trailing: nameLabel.leadingAnchor, padding: .init(top: screen.height * 0.075, left: screen.width * 0.15, bottom: 0, right: screen.width * 0.015), size: .init(width: 0, height: screen.height * 0.1))
         
@@ -107,7 +106,6 @@ class RePasswordViewController: UIViewController, UITextFieldDelegate {
         
     }
     
-    // MARK: - TextField Methods
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         emailTextField.resignFirstResponder()
         return true
@@ -116,5 +114,4 @@ class RePasswordViewController: UIViewController, UITextFieldDelegate {
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         self.view.endEditing(true)
     }
-
 }
